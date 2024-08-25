1a8fd2c53ccc34261c36293c09562f7c50b8f7ca
vismai05 <vismaimhatre05@gmail.com>
2024-08-25 22:47:06 +0530

    Created templates folder

--- Extractor/Extractor/urls.py
+++ Extractor/Extractor/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import function
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', function.index, name="index"),
 ]
